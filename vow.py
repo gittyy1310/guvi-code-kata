cdac47564ab914d26435089dbde41554fdc3c34d
gittyy1310 <47408826+gittyy1310@users.noreply.github.com>
2019-02-07 01:49:24 -0800

    Update vow.py

--- vow.py
+++ vow.py
@@ -1,8 +1,8 @@
-x=input('')
-a=x.isalpha()
-if(len(x)==1):
+b=input('')
+a=b.isalpha()
+if(len(b)==1):
 	if a==True:
-		if x in('a','e','i','o','u','A','E','I','O','U'):
+		if b in('a','e','i','o','u','A','E','I','O','U'):
 			print("Vowel")
 		else:
 			print("Consonant")
