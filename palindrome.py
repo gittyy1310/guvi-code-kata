ec21be7425c8c9ec771f90aaa59763e569b7dc0f
gittyy1310 <47408826+gittyy1310@users.noreply.github.com>
2019-02-20 00:33:52 -0800

    Create palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,12 @@
+def isPalindrome(s):
+    rev = ''.join(reversed(s)) 
+    if (s == rev): 
+        return True
+    return False
+s = "malayalam"
+ans = isPalindrome(s) 
+  
+if (ans): 
+    print("Yes") 
+else: 
+    print("No")
